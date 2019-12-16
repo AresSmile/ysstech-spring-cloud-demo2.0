b1d3900812ff251de6bb8c26ce86b3b41e555077
AresSmile <1047607279@qq.com>
2019-12-16 14:50:38 +0800

    add config

---
+++ demo/demo-config/src/test/java/com/ysstech/demo/config/DemoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysstech.demo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
